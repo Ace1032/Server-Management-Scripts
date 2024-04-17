faf8911d9ecb901d41ec8a48d31417cabcf93111
Imara Dharmakirthi <149819817+Ace1032@users.noreply.github.com>
2024-04-16 18:56:54 -0700

    Update disk_space.sh

--- disk_space.sh
+++ disk_space.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+#Suppose you want to monitor the disk usage of all partitions on your 
+#server and get alerted if any partition crosses a certain threshold (e.g., 80% usage). 
+
 df -h | awk 'NR >1 && $5>80 {print "WARNING: " $1 " is "$5 " full" }'
